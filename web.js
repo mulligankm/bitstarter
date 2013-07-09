453c83ce4afca59c8aeb5220be1afecf93075954
Kevin Mulligan <mulligankm@gmail.com>
2013-07-09 21:04:13 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync('index.html', 'UTF-8');
+console.log(buf.toString('UTF-8'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString('UTF-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
+
